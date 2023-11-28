352811b93fc25cc8be92401c7a3170905ee62dcc
Doan Huu Linh <doanlinh1510@gmail.com>
2023-11-28 11:07:20 +0700

    nop bai thi thuc hanh

---
+++ ReactHook/hook-use-effect/src/components/Selector.js
@@ -0,0 +1,42 @@
+/*Target: hiểu và s dụng được useEffect
+* Description: Sử dụng useEffect để lắng nghe sự thay đổi của biến và cập nhật giao diện tương ứng
+* Xây dựng component EffectDEMO, sử dụng hook useEffect
+*
+* tao folder components va tep Selector.js
+* trong Selector, them thu vien useState
+* tao component selector
+* Khởi tạo biến selected = 0 và setSelected với useState.
+* Khởi tạo biến valueSelected = “” và setValueSelected với useState.
+* tao ham handler
+* su dung useEffect de luu gia tri duoc chon
+* return component
+* export component selector
+* Su dung component selector trong component app
+* */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
